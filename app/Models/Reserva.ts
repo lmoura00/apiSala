ad8b35de851d2cea8ff383a7ee37548048f680c0
Lucas <lucasmoura53@gmail.com>
2024-09-27 17:38:20 -0300

    atualizado

--- app/Models/Reserva.ts
+++ app/Models/Reserva.ts
@@ -1,9 +1,17 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
+import {Docente} from 'app/models/docente'
+import {Sala} from 'app/models/sala'
 
 export default class Reserva extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @hasOne(() => Docente)
+  public docente: HasOne<typeof Docente>
+
+  @hasOne(() => Sala)
+  public sala: HasOne<typeof Sala>
 
   @column()
   public data_reserva: string
